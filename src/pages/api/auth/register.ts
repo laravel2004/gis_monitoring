c57014ab9e7de8d991264f6fdcbd93b75d8ac6d8
Sultan Firdaus <sultanfirdaus324@gmail.com>
2024-05-24 10:01:30 +0700

    image json

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -10,26 +10,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
 
-  if(req.method !== "POST"){
+  if (req.method !== "POST") {
     return res.status(405).json({ status: "error", message: "Method not allowed" });
   }
   const { name, email, password } = req.body;
-  try{
-    if(!name || !email || !password){
+  try {
+    if (!name || !email || !password) {
       return res.status(400).json({ status: "error", message: "Missing name, email or password" });
     }
     const hashedPassword = await hash(password, 10);
     const user = await prisma.user.create({
       data: {
-        name : name,
-        email : email,
+        name: name,
+        email: email,
         password: hashedPassword,
-        role : true
+        role: true
       }
     });
     return res.status(200).json({ status: "success", message: jwt.sign({ id: user.id }, process.env.JWT_SECRET!) });
   }
-  catch(err){
+  catch (err) {
     return res.status(500).json({ status: "error", message: "Internal server error" });
   }
 }
